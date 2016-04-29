7c82d5f9a459da8f6a5d12c36b503861f885554d
Carl Thuringer <carl@groupon.com>
2016-04-29 07:55:52 -0500

    Stuck again...

--- Library/Util.elm
+++ Library/Util.elm
@@ -5,9 +5,7 @@ import Debug exposing (watch)
 
 broadcast : List (Signal.Address d) -> d -> a -> Effects a
 broadcast outputs data action =
-  let check = watch "outputs" outputs
-  in
-    (List.map ((flip Signal.send) data) outputs)
-      |> (List.map Effects.task)
-      |> Effects.batch
-      |> Effects.map (always action)
+  (List.map ((flip Signal.send) data) outputs)
+    |> (List.map Effects.task)
+    |> Effects.batch
+    |> Effects.map (always action)
